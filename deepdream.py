319bd274478f86945912411016b371d3997f5596
Dave Townsend <dtownsend@oxymoronical.com>
2015-07-04 22:06:39 -0700

    Remove IPython dependencies Make deepdream.py loadable as a module Add a command line parser Don't save intermediate frames

--- deepdream.py
+++ deepdream.py
@@ -1,35 +1,43 @@
-#from cStringIO import StringIO
 import numpy as np
 import scipy.ndimage as nd
-import sys
+import os, sys
 import PIL.Image
-from IPython.display import clear_output, Image, display
 from google.protobuf import text_format
+
+model = 'bvlc_googlenet'
+layer = 'inception_4c/output'
+
+root = os.path.dirname(sys.argv[0])
+
+caffedir = os.path.join(root, 'caffe')
+sys.path.append(os.path.join(caffedir, 'python'))
 
 import caffe
 
+imagedir = os.path.join(root, 'output')
+
 def showarray(a, title, fmt='png'):
     a = np.uint8(np.clip(a, 0, 255))
-    #f = StringIO()
-    name = '/images/' + title + '.' + fmt
+    name = os.path.join(imagedir, title + '.' + fmt)
     PIL.Image.fromarray(a).save(name, fmt)
     print name
-    #display(Image(data=f.getvalue()))
 
-model_path = '/root/caffe/models/bvlc_googlenet/' # substitute your path here
-net_fn   = model_path + 'deploy.prototxt'
-param_fn = model_path + 'bvlc_googlenet.caffemodel'
+def loadnet(model=model):
+    model_path = os.path.join(caffedir, 'models', model) # substitute your path here
+    net_fn   = os.path.join(model_path, 'deploy.prototxt')
+    param_fn = os.path.join(model_path, model + '.caffemodel')
 
-# Patching model to be able to compute gradients.
-# Note that you can also manually add "force_backward: true" line to "deploy.prototxt".
-model = caffe.io.caffe_pb2.NetParameter()
-text_format.Merge(open(net_fn).read(), model)
-model.force_backward = True
-open('tmp.prototxt', 'w').write(str(model))
+    # Patching model to be able to compute gradients.
+    # Note that you can also manually add "force_backward: true" line to "deploy.prototxt".
+    model = caffe.io.caffe_pb2.NetParameter()
+    text_format.Merge(open(net_fn).read(), model)
+    model.force_backward = True
+    tmpfile = os.path.join(root, 'tmp/tmp.prototxt')
+    open(tmpfile, 'w').write(str(model))
 
-net = caffe.Classifier('tmp.prototxt', param_fn,
-    mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
-    channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
+    return caffe.Classifier(tmpfile, param_fn,
+                            mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
+                            channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
 
 # a couple of utility functions for converting to and from Caffe's input image layout
 def preprocess(net, img):
@@ -37,7 +45,7 @@ def preprocess(net, img):
 def deprocess(net, img):
     return np.dstack((img + net.transformer.mean['data'])[::-1])
 
-def make_step(net, step_size=1.5, end='inception_4c/output', jitter=32, clip=True):
+def make_step(net, end, step_size=1.5, jitter=32, clip=True):
     '''Basic gradient ascent step.'''
 
     src = net.blobs['data'] # input image is stored in Net's 'data' blob
@@ -77,26 +85,39 @@ def deepdream(net, base_img, end, iter_n=10, octave_n=4, octave_scale=1.4, clip=
         src.reshape(1,3,h,w) # resize the network's input image size
         src.data[0] = octave_base+detail
         for i in xrange(iter_n):
-            make_step(net, end=end, clip=clip, **step_params)
+            make_step(net, end, clip=clip, **step_params)
 
-            # visualization
-            vis = deprocess(net, src.data[0])
-            if not clip: # adjust image contrast if clipping is disabled
-                vis = vis*(255.0/np.percentile(vis, 99.98))
-            ename = '-'.join(end.split('/'))
-            showarray(vis, '{}-{}-{}'.format(ename, octave, i))
-            print octave, i, end, vis.shape
-            clear_output(wait=True)
+            # display step
+            #vis = deprocess(net, src.data[0])
+            #if not clip: # adjust image contrast if clipping is disabled
+            #    vis = vis*(255.0/np.percentile(vis, 99.98))
+            #ename = '-'.join(end.split('/'))
+            #showarray(vis, '{}-{}-{}'.format(octave, i))
+            #print octave, i, end, vis.shape
 
         # extract details produced on the current octave
         detail = src.data[0]-octave_base
     # returning the resulting image
     return deprocess(net, src.data[0])
 
-img = np.float32(PIL.Image.open('img.jpg'))
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Computer dreams')
+    parser.add_argument('image', metavar='IMAGE',
+                        help='the image to dream about')
+    parser.add_argument('--output', dest='output', default=None,
+                        help='the filename to save the result as')
+    parser.add_argument('--layer', dest='layer', default=layer,
+                        help='the layer to optimise')
+    args = parser.parse_args()
 
-end = 'inception_4c/output'
-if len(sys.argv) >= 2:
-    end = sys.argv[1]
+    img = np.float32(PIL.Image.open(args.image))
+    output = args.output
+    if output is None:
+        (name, extract) = os.path.splitext(args.image)
+        output = name + '_' + '-'.join(args.layer.split('/'))
 
-_=deepdream(net, img, end)
+    net = loadnet()
+    frame = deepdream(net, img, args.layer)
+
+    showarray(frame, output)
